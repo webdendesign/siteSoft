1f40e6ea6af0bd267ab1d26144a9aa30eed9f1b8
Den <fabri.din@yandex.ru>
2020-06-07 22:54:08 +0500

    slider, img, block, input fix

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
 
   var RadioBut = true;
-
   var slideNum = 1;
   var slideTime;
-  slideCount = $("#slider .slide").length;
+  slideCount = $("#slider-wrap .slide").length;
 
   var animSlide = function (arrow) {
     clearTimeout(slideTime);
@@ -13,19 +12,18 @@ $(document).ready(function () {
       if (slideNum == slideCount) {slideNum = 1;}
       else {slideNum++}
       translateWidth = -$('#active-slide').width() * (slideNum - 1);
-      $('#slider').css({'transform': 'translate(' + translateWidth + 'px, 0)'});
+      $('#slider-wrap').css({'transform': 'translate(' + translateWidth + 'px, 0)'});
     }
     else if (arrow == "prew") {
       if (slideNum == 1) {slideNum = slideCount;}
       else {slideNum -= 1}
       translateWidth = -$('#active-slide').width() * (slideNum - 1);
-      $('#slider').css({'transform': 'translate(' + translateWidth + 'px, 0)'});
+      $('#slider-wrap').css({'transform': 'translate(' + translateWidth + 'px, 0)'});
     } else {
       slideNum = arrow;
       translateWidth = -$('#active-slide').width() * (slideNum - 1);
-      $('#slider').css({'transform': 'translate(' + translateWidth + 'px, 0)'});
+      $('#slider-wrap').css({'transform': 'translate(' + translateWidth + 'px, 0)'});
     }
-
   }
 
   if (RadioBut) {
